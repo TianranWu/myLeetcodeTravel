85a93d1a549edb84432b98fa1252a608730779d3
T Wu <82632319+TianranWu@users.noreply.github.com>
2021-09-11 15:25:52 +0900

    Update LC226. Invert Binary Tree.py

--- LC226. Invert Binary Tree.py
+++ LC226. Invert Binary Tree.py
@@ -5,31 +5,46 @@ class TreeNode:
         self.left = left
         self.right = right
 
-class Solution:
-    def invertTree(self, root: TreeNode) -> TreeNode:
+# class Solution:
+#     def invertTree(self, root: TreeNode) -> TreeNode:
 
-        def backtrack(node):
+#         def backtrack(node):
             
-            if node.left == None and node.right == None:
+#             if node.right == None:
+#                 backtrack(node.left)
+#                 node.right = node.left
+#                 node.left = None
+#             elif node.left  == None:
+#                 backtrack(node.right)
+#                 node.left = node.right
+#                 node.right = None
+#             else:
+#                 backtrack(node.left)
+#                 backtrack(node.right)
+#                 temp = node.left
+#                 node.left = node.right
+#                 node.right = temp
+            
+#         if root == None:
+#             pass        
+#         else:
+#             backtrack(root)
+
+#         return root
+    
+    
+    class Solution:
+        def invertTree(self, root: TreeNode) -> TreeNode:
+
+            def backtrack(node):
+            
+            if node == None:
                 pass
-            elif node.right == None:
-                backtrack(node.left)
-                node.right = node.left
-                node.left = None
-            elif node.left  == None:
-                backtrack(node.right)
-                node.left = node.right
-                node.right = None
             else:
                 backtrack(node.left)
                 backtrack(node.right)
-                temp = node.left
-                node.left = node.right
-                node.right = temp
-            
-        if root == None:
-            pass        
-        else:
+                node.left, node.right = node.right, node.left
+                
             backtrack(root)
 
-        return root
+            return root
