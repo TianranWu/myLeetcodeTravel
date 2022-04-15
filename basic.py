cb5a9f03af8df8207c0a49c017cf81306677417c
CancerandFish <82632319+TianranWu@users.noreply.github.com>
2022-04-15 15:08:40 +0900

    1

---
+++ basic.py
@@ -0,0 +1,57 @@
+# class Node(object):
+#     def __init__(self,value):
+#         self.value = value
+#         self.next = None
+
+# class LinkedList(object):
+#     def __init__(self):
+#         return
+
+# class Queue(object):
+#     def __init__(self):
+#         self.item = []
+
+#     def inque(self, item):
+#         self.item.append(item)
+
+#     def dequeue(self):
+#         return self.item.pop(0)
+
+#     def empty(self):
+#         return True if self.item == [] else False
+    
+#     def size(self):
+#         return len(self.item)
+
+# class TreeNode(object):
+#     def __init__(self, val = 0, left = None, right = None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# class TrieNode(object):
+#     def __init__(self):
+#         self.children = [None] * 26
+#         self.is_word = False
+    
+# a = 1
+# b = 2
+# print(a, b)
+#  # swap a and b
+# # a, b = b, a
+# a = a+b
+# b = a-b
+# a = a-b
+# print(a,b)
+
+n = 3
+if n == 1:
+    print('*')
+else:
+    for i in range(n):
+        print(' * ' * (i+1))
+
+
+
+
+    
